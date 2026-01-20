ecf179cc90d62f91952152d1d532536c1a8e7930
IsaacJudeSAcera <isaacjude.acera@smc.pshs.edu.ph>
2026-01-20 10:33:23 +0800

    Add files via upload

---
+++ Acera_Q3_FA06.py
@@ -0,0 +1,10 @@
+student={
+
+}
+student['name']=input("Enter your name: ")
+student['age']=int(input("Enter your age: "))
+student['favorite subject']=input("Enter your favorite subject: ")
+print("Student record:")
+print("name:", student["name"])
+print("age:", student["age"])
+print("favorite subject:", student["favorite subject"])
